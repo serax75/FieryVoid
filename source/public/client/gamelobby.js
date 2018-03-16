7a975ca9179cc809a7d353f833d3de83b964dff2
Zero1627 <reisen@poczta.onet.pl>
2018-03-16 16:08:52 +0100

    Update gamelobby.js

--- source/public/client/gamelobby.js
+++ source/public/client/gamelobby.js
@@ -88,7 +88,7 @@ window.gamedata = {
 	    var jumpDrivePresent = false;
 	    var capitalShips = 0;
 	    var customShipPresent = false;
-	    var shipTable = array(); //ShipName->array(rarityLetter->count)
+	    var shipTable = new array(); //ShipName->array(rarityLetter->count)
 	    
 	    
 	    for (var i in gamedata.ships){
