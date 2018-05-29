1e40b3b1441db9d0e22e28de3ecbddcfacf91d60
Zero1627 <reisen@poczta.onet.pl>
2018-05-29 14:43:24 +0200

    Update Enhancements.php

--- source/server/model/ships/Enhancements.php
+++ source/server/model/ships/Enhancements.php
@@ -8,6 +8,8 @@ class Enhancements{
   called by setEnhancements if database is empty
   */
   public static function setEnhancementOptions($ship){
+	  //if an unit should have some non-standard enhancement options, they should be noted in ship SCS itself.
+	  
     $shid = $ship->id;
 	     $ship->enhancementOptions[] = array('TESTX','Test Enhancement Option X',0,2,125,0); //ID,readableName,numberTaken,limit,price,priceStep
 	     $ship->enhancementOptions[] = array('TESTV2','Test Enhancement Option Variable Price (central)',0,4,100,50);
@@ -21,7 +23,7 @@ class Enhancements{
   /*actually enhances unit (sets enhancement options if enhancements themselves are empty)
   */
   public static function setEnhancements($ship){
-    //if($ship->id < 1){
+    //if($ship->id < 1){ //if there are no relevant database entries; ship id is not a good check!
       Enhancements::setEnhancementOptions($ship);
     //}
     
