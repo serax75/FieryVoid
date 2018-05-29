a5e96f8a552479c82b9d05c1c647c70ee3baacc4
Zero1627 <reisen@poczta.onet.pl>
2018-05-29 14:34:13 +0200

    unit enhancements

--- source/server/controller/DBManager.php
+++ source/server/controller/DBManager.php
@@ -130,9 +130,9 @@ class DBManager {
 		}
 	}
 	
-	public function submitEnhancement($gameid, $ship, $enhid, $numbertaken, $enhname){	
+	public function submitEnhancement($gameid, $shipid, $enhid, $numbertaken, $enhname){	
 		try{
-			$sql = "INSERT INTO `B5CGM`.`tac_enhancements` VALUES($gameid, $ship, $enhid, $numbertaken, $this->DBEscape($enhname) )";
+			$sql = "INSERT INTO `B5CGM`.`tac_enhancements` VALUES($gameid, $shipid, $enhid, $numbertaken, $this->DBEscape($enhname) )";
 			$this->insert($sql);
 		}catch(Exception $e) {
 			$this->endTransaction(true);
