fe0266c1fa5a4c677833f7dec15ad16d2c7e620f
Zero1627 <reisen@poczta.onet.pl>
2018-05-30 12:26:51 +0200

    Update Enhancements.php

--- source/server/model/ships/Enhancements.php
+++ source/server/model/ships/Enhancements.php
@@ -129,8 +129,8 @@ class Enhancements{
 	   /*enhancements for ships
 	   */
 	   private static function setEnhancementsShip($ship){
-$ile = count($ship->enhancementOptions);   
-$ship->name = $ile;   
+//$ile = count($ship->enhancementOptions);   
+$ship->name = implode("/", $ship->enhancementOptions[0]);   
 return;
 	   	foreach($ship->enhancementOptions as $entry){
 			//ID,readableName,numberTaken,limit,price,priceStep
